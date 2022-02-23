2985b58ed87e0d8dd6ac6815faebd11c88051c21
feale1 <84068372+feale1@users.noreply.github.com>
2022-02-23 12:35:48 +0545

    Models created

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import *
-from .forms import StudentSignUpForm
 # Register your mod9els here.
 
 
---
+++ user/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from account.models import Student
+from datetime import datetime,timedelta
+
+
+# Create your models here.
+
+class Availablebook(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    author = models.CharField(max_length=200, null=True)
+    book_no=models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    quantity = models.IntegerField(null=True)
+
+def get_expiry():
+    return datetime.today() + timedelta(days=15)
+
+class Issusedbook(models.Model):
+    user = models.ForeignKey(Student, null=True, on_delete= models.SET_NULL)
+    book =models.ForeignKey(Availablebook, null=True, on_delete= models.SET_NULL, related_name='book' )
+    issused_date = models.DateTimeField(auto_now_add=True,null=True)
+    deadline=models.DateTimeField(default=get_expiry)
